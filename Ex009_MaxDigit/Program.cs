9d8d62bd0df80d33da01b6a9873935fcf056db59
Sergey_Podgornov <psv_23@mail.ru>
2022-10-17 20:51:10 +0300

    Задача 9. Наибольшая цифра числа от 10 до 99

---
+++ Ex009_MaxDigit/Program.cs
@@ -0,0 +1,13 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+using System;
+
+Console.Clear();
+int number = new Random().Next(10, 100);
+Console.WriteLine("Дано число: " + number);
+int digit1 = number / 10;
+int digit2 = number % 10;
+int max = digit1;
+if (digit2 > max){
+    max = digit2;
+}
+Console.WriteLine("Наибольшая цифра: " + max);
