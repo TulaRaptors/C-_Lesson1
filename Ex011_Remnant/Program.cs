2c6d415abf7e1250d602e076868b86b94cc376cd
Sergey_Podgornov <psv_23@mail.ru>
2022-10-17 20:26:23 +0300

    Задача 11. Кратно ли число заданному

---
+++ Ex011_Remnant/Program.cs
@@ -0,0 +1,15 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+using System;
+
+Console.Clear();
+int a = 3;
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % a == 0) {
+    Console.WriteLine("Число кратно " + a);
+}
+else{
+    Console.Write("Число не кратно " + a + ", остаток от деления: ");
+    Console.WriteLine(number % a);
+}
