5357cc2f2a5b0abab4bf3dcb29a07bb1144f490c
Sergey_Podgornov <psv_23@mail.ru>
2022-10-17 17:30:01 +0300

    Задача 7. Последняя цифра трехзначного числа

---
+++ Ex007_LastDigit/Program.cs
@@ -0,0 +1,16 @@
+﻿// Показать последнюю цифру трёхзначного числа
+
+using System;
+
+Console.Clear();
+Console.Write("Введите трехзначное число: ");
+int number = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+string str = Convert.ToString(number);
+
+if (str.Length  == 3) {
+    int lastdigit = str[str.Length - 1] - '0';
+    Console.WriteLine("Последняя цифра числа: " + lastdigit);
+}
+else{
+    Console.WriteLine("Число не является трехзначным!");
+}
