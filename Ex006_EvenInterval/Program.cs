266ddbd2d05f8ad1d63ce868e1380abd9705ace0
Sergey_Podgornov <psv_23@mail.ru>
2022-10-17 14:17:57 +0300

    Задача 6. Чётные числа от 1 до N

---
+++ Ex006_EvenInterval/Program.cs
@@ -0,0 +1,12 @@
+﻿// Показать четные числа от 1 до N
+
+using System;
+
+Console.Clear();
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+while (count < (N+1)){
+    Console.Write(count + ", ");
+    count = count + 2;
+}
