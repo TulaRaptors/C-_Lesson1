e57621998b95cc1e1605f660c7da3f100af96c6d
Sergey_Podgornov <psv_23@mail.ru>
2022-10-17 13:50:36 +0300

    Задача 4. Проверка чётное ли число

---
+++ Ex004_EvenNumber/Program.cs
@@ -0,0 +1,13 @@
+﻿// Выяснить является ли число чётным
+
+using System;
+
+Console.Clear();
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if ((number % 2) == 0){
+    Console.WriteLine("Число ЧЁТНОЕ");
+}
+else{
+    Console.WriteLine("Число НЕЧЁТНОЕ");
+}
