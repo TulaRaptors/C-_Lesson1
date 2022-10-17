a7c5ff8bc91193137670c9bb7055f957b58b2a31
Sergey_Podgornov <psv_23@mail.ru>
2022-10-17 13:30:58 +0300

    Задача 2. День недели по номеру

---
+++ Ex002_Week/Program.cs
@@ -0,0 +1,34 @@
+﻿// По заданному номеру дня недели вывести его название
+
+using System;
+
+Console.Clear();
+Console.Write("Введите номер дня недели: ");
+int number = Convert.ToInt16(Console.ReadLine());
+
+if (number >=1 && number <=7) {
+    if (number == 1){
+        Console.WriteLine("ПОНЕДЕЛЬНИК");
+    }
+    if (number == 2){
+        Console.WriteLine("ВТОРНИК");
+    }
+    if (number == 3){
+        Console.WriteLine("СРЕДА");
+    }
+    if (number == 4){
+        Console.WriteLine("ЧЕТВЕРГ");
+    }
+    if (number == 5){
+        Console.WriteLine("ПЯТНИЦА");
+    }
+    if (number == 6){
+        Console.WriteLine("СУББОТА");
+    }
+    if (number == 7){
+        Console.WriteLine("ВОСКРЕСЕНЬЕ");
+    }
+}
+else{
+    Console.WriteLine("Такого дня недели не существует!");
+}
