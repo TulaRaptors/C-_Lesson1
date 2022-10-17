6a740c3168c75a9ef363d1b74a4f736e3a4408fb
Sergey_Podgornov <psv_23@mail.ru>
2022-10-17 17:46:48 +0300

    Задача 8. Вторая цифра трехзначного числа

---
+++ Ex008_SecondDigit/Program.cs
@@ -0,0 +1,15 @@
+﻿// Показать вторую цифру трёхзначного числа
+
+using System;
+
+Console.Clear();
+Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string str = Convert.ToString(Math.Abs(number));
+
+if (str.Length  == 3) {
+    Console.WriteLine("Вторая цифра числа " + number + ": " + str[1]);
+}
+else{
+    Console.WriteLine("Число не является трехзначным!");
+}
