4da876abb0dbf7c86745376e7d9a98059bd88ac6
Sergey_Podgornov <psv_23@mail.ru>
2022-10-17 12:52:15 +0300

    Задача 1. Проверка квадрата числа

---
+++ Ex001_Square/Program.cs
@@ -0,0 +1,17 @@
+﻿// По двум заданным числам проверять является ли первое квадратом второго
+using System;
+
+Console.Clear();
+
+Console.Write("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA == Math.Pow(numberB, 2))
+{
+    Console.WriteLine("Первое число является квадратом второго");
+}
+else
+{
+    Console.WriteLine("Первое число НЕ квадрат второго");
+}
