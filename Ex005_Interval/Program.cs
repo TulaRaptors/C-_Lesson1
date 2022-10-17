4dbba25d4c937bb0c5fd368eb97e63acb1827fa4
Sergey_Podgornov <psv_23@mail.ru>
2022-10-17 14:10:51 +0300

    Задача 5. Числа в интервале от -N до N

---
+++ Ex005_Interval/Program.cs
@@ -0,0 +1,13 @@
+﻿// Показать числа от -N до N
+
+using System;
+
+Console.Clear();
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = -N;
+while (count < (N+1)){
+    Console.Write(count + ", ");
+    count++;
+}
+
