13ba4622ba00db5e570dc177ac706471a248936b
Sergey_Podgornov <psv_23@mail.ru>
2022-10-17 18:09:11 +0300

    Задача 12. Третья цифра числа

---
+++ Ex012_ThirdDigit/Program.cs
@@ -0,0 +1,15 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+
+using System;
+
+Console.Clear();
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string str = Convert.ToString(Math.Abs(number));
+
+if (str.Length >= 3) {
+    Console.WriteLine("Третья цифра числа " + number + ": " + str[2]);
+}
+else{
+    Console.WriteLine("В числе меньше трех цифр!");
+}
