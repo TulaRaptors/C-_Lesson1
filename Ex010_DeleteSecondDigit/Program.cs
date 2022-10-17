881f30d840a5ea14a600b51b9b22693a628c7be2
Sergey_Podgornov <psv_23@mail.ru>
2022-10-17 18:28:16 +0300

    Задача 10. Удалить вторую цифру трехзначного числа

---
+++ Ex010_DeleteSecondDigit/Program.cs
@@ -0,0 +1,14 @@
+﻿// Удалить вторую цифру трёхзначного числа
+using System;
+
+Console.Clear();
+Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string str = Convert.ToString(Math.Abs(number));
+
+if (str.Length  == 3) {
+    Console.WriteLine("После удаления второй цифры: " + str.Remove(1,1));
+}
+else{
+    Console.WriteLine("Число не является трехзначным!");
+}
